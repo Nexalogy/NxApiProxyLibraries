38740797847370e86b623e469ec4cb06a707c091
Lanny Rivest <lawfulkami@gmail.com>
2021-11-11 15:04:25 -0500

    Revert "[master] Automatic proxy sync by kami"
    This reverts commit 06ba3fb29940d2be950a37c2fb0d0171587902b1.

--- NxApiProxy.php
+++ NxApiProxy.php
@@ -1,1 +1,45 @@
-You have an empty or invalid base URL. The baseURL value must be set in Config\App.php, or through the .env file.
+{
+    "title": "ErrorException",
+    "type": "ErrorException",
+    "code": 500,
+    "message": "Undefined variable: apiKey",
+    "file": "/var/www/app/Controllers/proxyController.php",
+    "line": 110,
+    "trace": [
+        {
+            "file": "/var/www/app/Controllers/proxyController.php",
+            "line": 110,
+            "function": "errorHandler",
+            "class": "CodeIgniter\\Debug\\Exceptions",
+            "type": "->"
+        },
+        {
+            "file": "/var/www/system/CodeIgniter.php",
+            "line": 918,
+            "function": "php",
+            "class": "App\\Controllers\\proxyController",
+            "type": "->"
+        },
+        {
+            "file": "/var/www/system/CodeIgniter.php",
+            "line": 404,
+            "function": "runController",
+            "class": "CodeIgniter\\CodeIgniter",
+            "type": "->"
+        },
+        {
+            "file": "/var/www/system/CodeIgniter.php",
+            "line": 312,
+            "function": "handleRequest",
+            "class": "CodeIgniter\\CodeIgniter",
+            "type": "->"
+        },
+        {
+            "file": "/var/www/public/index.php",
+            "line": 82,
+            "function": "run",
+            "class": "CodeIgniter\\CodeIgniter",
+            "type": "->"
+        }
+    ]
+}
